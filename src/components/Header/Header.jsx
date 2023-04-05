22464cb7eceff7a99623a189cbb5757038ab9737
rajib-sadhu <rahulrajib.sadhu@gmail.com>
2023-04-05 16:28:22 +0530

    add routes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,16 +3,19 @@ import './Header.css';
 import logo from  '../../images/Logo.svg';
 
 import {BsFillCartFill} from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav className='header'>
+            <Link to='/'>
             <img src={logo} alt="" />
+            </Link>
             <div className='links'>
-                <a href="#order">Order</a>
-                <a href="#OrderReview">Order Review</a>
-                <a href="#ManageInventory">Manage Inventory</a>
-                <a href="#Login">Login</a>
+                <Link  to="/orders">Orders</Link>
+                <Link  to="#">Order Review</Link>
+                <Link  to="/inventory">Manage Inventory</Link>
+                <Link  to="/Login">Login</Link>
             </div>
             <div className='header-cart'>
                 <BsFillCartFill/>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Shop from './components/Shop/Shop'
+import Home from './components/Layout/Home'
+import Orders from './components/Orders/Orders'
+import Inventory from './components/Inventory/Inventory'
+import Login from './components/Login/Login'
+
+const router = createBrowserRouter([
+
+  {
+    path:'/',
+    element:<Home/>,
+    errorElement:'',
+    children:[
+      {
+        path:'/',
+        element:<Shop/>
+      },
+      {
+        path:'orders',
+        element:<Orders/>
+      },
+      {
+        path:'inventory',
+        element:<Inventory/>
+      },
+      {
+        path:'login',
+        element:<Login/>
+      },
+    ]
+  }
+
+])
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
